844e794a5b3130ba549199200e38ee4bd7d8ba0e
Hrishikesh-3459 <hrishikeshmm01@gmail.com>
2020-07-18 16:21:21 +0530

    Created db_config

--- app.py
+++ app.py
@@ -1,38 +1,17 @@
 import mysql.connector
 from werkzeug.security import check_password_hash, generate_password_hash
+from db_config import dbMysql
 
-mydb = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    passwd="<<password>>"
-    database="library"
-)
-
+db = dbMysql()
+mydb = db.connection()
 mycursor = mydb.cursor(buffered=True)
+db.configure_db(mycursor)
 
-user = "<<username>>"
-password = "<<password>>"
+user = input("Enter username: ")
+password = input("Enter Password: ")
 hash_pas = generate_password_hash(password, method='pbkdf2:sha256', salt_length=8)
-print(hash_pas)
+
 
 mycursor.execute("INSERT INTO users (username, password) VALUES (%s, %s)", (user, hash_pas))
 mydb.commit()
-
-# CREATE TABLE `users` (
-#   `id` INT NOT NULL AUTO_INCREMENT,
-#   `username` VARCHAR(50) NOT NULL,
-#   `password` VARCHAR(256) NOT NULL,
-#   `money` INT NULL DEFAULT 1000.00,
-#   PRIMARY KEY (`id`)
-# )
-
-# CREATE TABLE register (
-# 	serial_no INT NOT NULL AUTO_INCREMENT,
-#     user_id INT,
-#     book_name VARCHAR(50) NOT NULL,
-#     borrowed TIMESTAMP,
-#     returned TIMESTAMP,
-#     PRIMARY KEY (serial_no),
-#     FOREIGN KEY (user_id) REFERENCES users(id)
-#     );
 
---
+++ db_config.py
@@ -0,0 +1,49 @@
+import mysql.connector
+
+
+class dbMysql():
+    def __init__(self):
+        self.pwd = "<password>"
+        self.username = "root"
+        self.host = "localhost"
+        self.database = "library"
+        self.mydb = None
+
+    def connection(self):
+        self.mydb = mysql.connector.connect(
+            host=self.host,
+            user=self.username,
+            passwd=self.pwd,
+            database=self.database
+        )
+        return(self.mydb)
+
+    def configure_db(self, mycursor):
+        mycursor.execute("CREATE DATABASE IF NOT EXISTS library")
+        self.mydb.commit()
+        mycursor.execute(
+            "CREATE TABLE IF NOT EXISTS users (user_id INT NOT NULL AUTO_INCREMENT, username VARCHAR(255) NOT NULL, password VARCHAR(256) NOT NULL, money INT NULL DEFAULT 1000.00, PRIMARY KEY (user_id))")
+        self.mydb.commit()
+        mycursor.execute(
+            "CREATE TABLE IF NOT EXISTS register (serial_no INT NOT NULL AUTO_INCREMENT, user_id int, book_name VARCHAR(50) NOT NULL, borrowed TIMESTAMP, returned TIMESTAMP, PRIMARY KEY (serial_no), FOREIGN KEY (user_id) REFERENCES users(user_id))")
+        self.mydb.commit()
+
+
+# CREATE TABLE `users` (
+#   `id` INT NOT NULL AUTO_INCREMENT,
+#   `username` VARCHAR(50) NOT NULL,
+#   `password` VARCHAR(256) NOT NULL,
+#   `money` INT NULL DEFAULT 1000.00,
+#   PRIMARY KEY (`id`)
+# )
+
+# CREATE TABLE register (
+# 	serial_no INT NOT NULL AUTO_INCREMENT,
+#     user_id INT,
+#     book_name VARCHAR(50) NOT NULL,
+#     borrowed TIMESTAMP,
+#     returned TIMESTAMP,
+#     PRIMARY KEY (serial_no),
+#     FOREIGN KEY (user_id) REFERENCES users(id)
+#     );
+
